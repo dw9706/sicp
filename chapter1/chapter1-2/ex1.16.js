e0ba827615c30f947ecef315ac45ddde9603502a
dw9706 <dw9706@gmail.com>
2022-02-23 17:59:47 +0900

    second commit

--- chapter1/chapter1-2/ex1.16.js
+++ chapter1/chapter1-2/ex1.16.js
@@ -11,15 +11,15 @@ function is_even(n) {
 
 
 function fast_expt_iter(a, b, n) {
-    return n === 1
-    ? a
+    return n === 0
+    ? a 
     : is_even(n)
-    ? fast_expt(square(b), b, n / 2)
-    : b * fast_expt(b, n - 1);
+    ? fast_expt_iter(a, square(b), n / 2)
+    : fast_expt_iter( a*b , b , n - 1);
 }
 
 function fast_expt(b,n){
     return fast_expt_iter(1,b,n)
 }
 
-console.log(fast_expt(3, 4));
+console.log(fast_expt(7, 5));
